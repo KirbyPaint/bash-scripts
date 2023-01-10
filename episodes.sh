f32187032777fb408e1dd3858ddc06a39c3a215b
Ash Porter <porter.ashley13@gmail.com>
2023-01-09 23:13:53 -0800

    update script

--- episodes.sh
+++ episodes.sh
@@ -3,3 +3,4 @@
 set -euo pipefail
 
 ls -aR /media/plex/media/tv | egrep '\.mp4$|\.mkv$' > /home/plex/Desktop/bot/episodeList.txt
+ls -aR /media/plex/media/movies | egrep '\.mp4$|\.mkv$' > /home/plex/Desktop/bot/movieList.txt
