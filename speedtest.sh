7a52cde46d5bee9ee6d3722a930167309edd1db9
Ash Porter <porter.ashley13@gmail.com>
2023-03-25 19:14:27 -0700

    update

--- speedtest.sh
+++ speedtest.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-speedtest-cli --json >> "/home/plex/Documents/speedtest/json/speedtest_$(date +%Y%m%d_%H%M%S).json"
+speedtest-cli --json >> "/home/plex/Documents/speedtest/json/speedtest_$(date +%Y%m%d%H%M%S).json"
